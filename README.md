8726b88a0b413f7139ff44cea93c00c22af0788a
Enrikrfg <enrikrfg@gmail.com>
2024-02-28 22:48:09 -0600

    Segunda versión

--- README.md
+++ README.md
@@ -1,2 +1,4 @@
 # Project5
 Project 5 for DataScience Bootcamp
+Se realiza una aplicación en la que muestra un analisis grafico 
+de las marcas de carros más utilizadas en EUA
--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 import pandas as pd
 import plotly.express as px
 import streamlit as st
-        
+
+st.header('Datos sobre carros en USA')
+
 car_data = pd.read_csv('/Users/ramsesenriquefloresgomez/Desktop/DATASCIENTIST/Projecto 5/Project5/vehicles_us.csv') # leer los datos
 hist_button = st.button('Construir histograma') # crear un botón
         
@@ -16,10 +18,27 @@ if hist_button: # al hacer clic en el botón
     st.plotly_chart(fig, use_container_width=True)
 
 
-import streamlit as st
+
+disp_button = st.button('Construir gráfico de dispersión') # crear un botón
+
+if disp_button: 
+    
+    st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
+    
+    fig = px.scatter(car_data, x="odometer", y="price") # crear un gráfico de dispersión
+    
+    st.plotly_chart(fig, use_container_width=True) # crear gráfico de dispersión
+
+
 
 # crear una casilla de verificación
 build_histogram = st.checkbox('Construir un histograma')
 
 if build_histogram: # si la casilla de verificación está seleccionada
     st.write('Construir un histograma para la columna odómetro')
+            
+            # crear un histograma
+    fig = px.histogram(car_data, x="odometer")
+        
+            # mostrar un gráfico Plotly interactivo
+    st.plotly_chart(fig, use_container_width=True)
